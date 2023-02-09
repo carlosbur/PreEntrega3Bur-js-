1f6968c7ef01419919559b6ed814efaa45895b9c
carlosbur <carlosbur@gmail.com>
2023-02-09 18:21:43 -0300

    Trabajé en el carrito de compras

--- JS/main.js
+++ JS/main.js
@@ -1,12 +1,29 @@
 // Capturo los elementos que necesito para mi JS
 let productosDiv = document.getElementById("productosDiv")
-let verCatalogobtn = document.getElementById("verCatalogo")
-let ocultarCatalogobtn = document.getElementById("ocultarCatalogo")
+// let verCatalogobtn = document.getElementById("verCatalogo")
+// let ocultarCatalogobtn = document.getElementById("ocultarCatalogo")
 let btnDark = document.getElementById("btnDark")
 let inputBuscador = document.getElementById("buscador")
 let coincidencia = document.getElementById("coincidencia")
 let selectOrden = document.getElementById("selectOrden")
+let botonCarrito = document.getElementById("botonCarrito")
+let modalCarrito = document.getElementById("modal-bodyCarrito")
+let precioTotal = document.getElementById("precioTotal")
 
+
+
+
+// Array productos en Carrito
+
+let productosCarrito
+if(localStorage.getItem("Carrito")){
+
+    productosCarrito = JSON.parse(localStorage.getItem("Carrito"))
+
+}else{
+    productosCarrito = []
+    localStorage.setItem("Carrito", productosCarrito)
+}
 
 // Funciones
 
@@ -29,11 +46,21 @@ function verCatalogo(array){
                 <button id="agregarBtn${producto.id}" class="btn btn-outline-success">Comprar</button>
             </div>
         </div> 
-        `        
+        `    
         productosDiv.appendChild(nuevoProductodiv)
-    }
+        
+        let agregarBtn = document.getElementById(`agregarBtn${producto.id}`)
+        console.log(agregarBtn)
+        agregarBtn.onclick = ()=>{
+            agregarCarrito(producto)
+
+    }    
+
 }
 
+}
+
+// Función para agregar nuevos productos
 
 function agregarProducto(array){
     let nombreProducto = document.getElementById("productoInput")  
@@ -52,6 +79,7 @@ function agregarProducto(array){
     formAgregarProducto.reset()
 }
 
+// Función para buscar
 function buscarProd(buscado, array){
     let busqueda = array.filter(
         (prod) => prod.nombreProducto.toLowerCase().includes(buscado.toLowerCase()))
@@ -101,6 +129,59 @@ function ordenAlfa(array){
     verCatalogo(alfabetico)
 }
 
+// Función para agregar al carrito
+
+function agregarCarrito(producto){
+    
+    // evaluamos si existe el producto
+    let prodYaAgregado = productosCarrito.find((prod)=> prod.id == producto.id)
+
+    if (prodYaAgregado == undefined){
+        // pusheo productos al carrito
+        productosCarrito.push(producto)
+        // seteo en el storage
+        localStorage.setItem("Carrito", JSON.stringify(productosCarrito))
+    }else{
+
+    alert(`El Producto ${producto.nombreProducto} ya se encuentra en el Carrito!!!`)
+}
+}
+
+
+// Función para agregar productos al carrito de compras
+function cargarCarrito(array){
+    modalCarrito.innerHTML = ""
+    array.forEach((prodCarrito)=>{
+        
+        modalCarrito.innerHTML += `
+        <div class="card border-primary mb-3" id ="productosCarrito${prodCarrito.id}" style="max-width: 540px;">
+            <img class="card-img-top" height="300px" src="img/${prodCarrito.imagen}" alt="${prodCarrito.nombreProducto}">
+            <div class="card-body">
+                    <h4 class="card-title">${prodCarrito.nombreProducto}</h4>
+                
+                    <p class="card-text">$${prodCarrito.precio}</p> 
+                    <button class= "btn btn-danger" id="botonEliminar${prodCarrito.id}"><i class="fas fa-trash-alt"></i></button>
+            </div>    
+        </div>
+        `
+        })
+        compraTotal(array)
+    }
+    
+
+// Función para calcular la compra total
+
+function compraTotal(array){
+    let acumulador = 0
+    for (let producto of array){
+        acumulador += producto.precio
+    }
+    precioTotal.innerHTML = `El precio total es $ ${acumulador}`
+    return acumulador
+} 
+
+
+
 // Eventos
 
 guardarProductoBtn.addEventListener("click", ()=>{
@@ -109,20 +190,20 @@ guardarProductoBtn.addEventListener("click", ()=>{
     
 })
 
-verCatalogobtn.onclick = ()=> {
-    verCatalogo(listaProductos)
-}
+// verCatalogobtn.onclick = ()=> {
+//     verCatalogo(listaProductos)
+// }
 
-ocultarCatalogobtn.onclick = ()=>{
-    productosDiv.innerHTML = `<h3> Haga click en Ver Catálogo para consultar nuestros productos<h3>`
-}
+// ocultarCatalogobtn.onclick = ()=>{
+//     productosDiv.innerHTML = `<h3> Haga click en Ver Catálogo para consultar nuestros productos<h3>`
+// }
 
 // Evento Buscador 
 inputBuscador.addEventListener("input", ()=> {
     buscarProd(inputBuscador.value, listaProductos)  
 })
 
-// Eventos ordenar productos
+// Evento ordenar productos
 
 selectOrden.addEventListener("change", ()=>{
     if(selectOrden.value == "1"){
@@ -134,4 +215,11 @@ selectOrden.addEventListener("change", ()=>{
     }
 })
 
+botonCarrito.addEventListener("click", ()=> {
+    cargarCarrito(productosCarrito)
+})
 
+
+// Código
+
+verCatalogo(listaProductos)
